9af49b62759afa1fb0443c342cd2599b90ea3287
Andrei Asi <johnwayneandreiasi@gmail.com>
2022-07-03 21:43:04 +0800

    Add Player Input

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 let characters = Array("rock", "paper", "scissors");
 
-function computerPlay() {
-  let computerAnswer = characters[Math.floor(Math.random() * characters.length)];
-  console.log(computerAnswer);
+function computerPlay(){
+    let computerAnswer = characters[Math.floor(Math.random() * characters.length)];
+    console.log(computerAnswer);
+}
+
+function playerInput(){
+    let playerAnswer = prompt("Rock, Paper, Scissors!!")
+    playerAnswer = playerAnswer.toLowerCase();
+
+    if (characters.indexOf(playerAnswer) !== -1){
+        console.log(playerAnswer);
+        return (playerAnswer);
+    }
+    else {
+        playerInput();
+    }
 }
 
 computerPlay();
+playerInput();
