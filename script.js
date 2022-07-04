e186b6b6b4bd306ac28279413ecf4c155b419666
Andrei Asi <johnwayneandreiasi@gmail.com>
2022-07-04 12:00:53 +0800

    Add playRound function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let characters = Array("rock", "paper", "scissors");
 
 function computerPlay(){
     let computerAnswer = characters[Math.floor(Math.random() * characters.length)];
-    console.log(computerAnswer);
+    return computerAnswer;
 }
 
 function playerInput(){
@@ -14,9 +14,34 @@ function playerInput(){
         return (playerAnswer);
     }
     else {
+        console.log("Invalid input. Type rock paper or scissors.")
         playerInput();
     }
 }
 
-computerPlay();
-playerInput();
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection == computerSelection) {
+        console.log("A Draw!")
+    }
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
+        console.log("You Win!")
+        return true;
+    }
+    else if (playerSelection == "rock" && computerSelection == "scissors") {
+        console.log("You Win!")
+        return true;
+
+    }
+    else if (playerSelection == "paper" && computerSelection == "rock") {
+        console.log("You Win!")
+        return true;
+    }
+    else {
+        console.log("Computer Wins!")
+        return false;
+    }
+}
+
+
+playRound(playerInput(),computerPlay());
